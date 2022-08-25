13428fa029c799affc0a6e8c953093bedb8a22e5
Zdravko <zdravko.kardzhaliyski@gmail.com>
2022-08-25 23:03:26 +0300

    Implemented ArrayList

--- Fundamentals/_01_LinearDataStructures/src/main/java/implementations/ArrayList.java
+++ Fundamentals/_01_LinearDataStructures/src/main/java/implementations/ArrayList.java
@@ -2,58 +2,136 @@ package implementations;
 
 import interfaces.List;
 
-import java.util.Arrays;
 import java.util.Iterator;
 
 public class ArrayList<E> implements List<E> {
+    private static final int INITIAL_CAPACITY = 4;
+    private Object[] elements;
+    private int capacity;
+    private int size;
+
+    public ArrayList() {
+        this.capacity = INITIAL_CAPACITY;
+        this.elements = new Object[capacity];
+        this.size = 0;
+    }
 
     @Override
     public boolean add(E element) {
-        return false;
+        ensureCapacity();
+
+        elements[size++] = element;
+        return true;
     }
 
     @Override
     public boolean add(int index, E element) {
-        return false;
+        validIndex(index);
+        ensureCapacity();
+
+        shiftRight(index);
+        elements[index] = element;
+        return true;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        validIndex(index);
+        return (E) elements[index];
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        validIndex(index);
+        Object tmp = elements[index];
+        elements[index] = element;
+        return (E) tmp;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        validIndex(index);
+
+        Object tmp = elements[index];
+        shiftLeft(index);
+        return (E) tmp;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public int indexOf(E element) {
-        return 0;
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(element)) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     @Override
     public boolean contains(E element) {
-        return false;
+        return indexOf(element) >= 0;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            private int currentIndex = 0;
+            @Override
+            public boolean hasNext() {
+                return currentIndex < size;
+            }
+
+            @Override
+            public E next() {
+                return get(currentIndex++);
+            }
+        };
+    }
+
+    private void ensureCapacity() {
+        if (capacity == size) {
+            grow();
+        }
+    }
+
+    private void grow() {
+        capacity *= 2;
+        Object[] newArray = new Object[capacity];
+        System.arraycopy(elements, 0, newArray, 0, size);
+        elements = newArray;
+    }
+
+
+    private void shiftRight(int index) {
+        for (int i = size; i >= index; i--) {
+            elements[i + 1] = elements[i];
+        }
+
+        size++;
+    }
+
+    private void shiftLeft(int index) {
+        for (int i = index; i < size; i++) {
+            elements[i] = elements[i + 1];
+        }
+
+        size--;
+    }
+
+    private void validIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index should be in list boundaries! For: " + index);
+        }
     }
 }
