f2990c0516897b0f8d26523db6dab8d20d2137c0
Zdravko <zdravko.kardzhaliyski@gmail.com>
2022-08-26 14:13:47 +0300

    Implemented Queue and Stack

--- Fundamentals/_01_LinearDataStructures/src/main/java/implementations/Queue.java
+++ Fundamentals/_01_LinearDataStructures/src/main/java/implementations/Queue.java
@@ -5,34 +5,78 @@ import interfaces.AbstractQueue;
 import java.util.Iterator;
 
 public class Queue<E> implements AbstractQueue<E> {
+    private Node<E> head;
+    private int size;
+
+    public Queue() {
+        size = 0;
+    }
 
     @Override
     public void offer(E element) {
+        Node<E> newNode = new Node<>(element);
 
+        if (head == null) {
+            head = newNode;
+            size++;
+            return;
+        }
+
+        Node<E> current = head;
+        while (current.getNext() != null) {
+            current.setNext(newNode);
+        }
+
+        size++;
     }
 
     @Override
     public E poll() {
-        return null;
+        if (head == null) {
+            throw new IllegalStateException("Queue must not be empty!");
+        }
+
+        Node<E> toRemove = head;
+        head = head.getNext();
+        size--;
+        return toRemove.getValue();
     }
 
     @Override
     public E peek() {
-        return null;
+        if (head == null) {
+            throw new IllegalStateException("Queue must not be empty!");
+        }
+
+        return head.getValue();
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            Node<E> current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                E value = current.getValue();
+                current = current.getNext();
+                return value;
+            }
+        };
     }
 }
--- Fundamentals/_01_LinearDataStructures/src/main/java/implementations/Stack.java
+++ Fundamentals/_01_LinearDataStructures/src/main/java/implementations/Stack.java
@@ -5,34 +5,67 @@ import interfaces.AbstractStack;
 import java.util.Iterator;
 
 public class Stack<E> implements AbstractStack<E> {
+    private Node<E> head;
+    private int size;
+
+    public Stack() {
+        size = 0;
+    }
 
     @Override
     public void push(E element) {
-
+        Node<E> newNode = new Node<>(element);
+        newNode.setNext(head);
+        head = newNode;
+        size++;
     }
 
     @Override
     public E pop() {
-        return null;
+        if(head == null) {
+            throw new IllegalStateException("Cant pop from empty stack!");
+        }
+
+        Node<E> temp = head;
+        head = head.getNext();
+        size--;
+        return temp.getValue();
     }
 
     @Override
     public E peek() {
-        return null;
+        if(head == null) {
+            throw new IllegalStateException("Cant pop from empty stack!");
+        }
+
+        return head.getValue();
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            Node<E> current = head;
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                E value = current.getValue();
+                current = current.getNext();
+                return value;
+            }
+        };
     }
 }
